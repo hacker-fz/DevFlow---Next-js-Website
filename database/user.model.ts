2509e3c3af243d8e224cd289c866b54515c87c46
Faiz <220701167@rajalakshmi.edu.in>
2025-02-10 13:32:55 +0530

    implementing user and account model

---
+++ database/user.model.ts
@@ -0,0 +1,30 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  email: string;
+  username: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String, required: true },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const user = models.User || model<IUser>("User", UserSchema);
+
+export default user;
