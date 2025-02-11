060f2f25da2e5b0531192632a916425c48b4bf9e
Faiz <220701167@rajalakshmi.edu.in>
2025-02-11 13:36:09 +0530

    implement user API routes for GET and POST

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 interface Tag {
   _id: string;
   name: string;
@@ -19,3 +21,19 @@ interface Question {
   views: number;
   answers: number;
 }
+
+type ActionResponse<T = null> = {
+  success: boolean;
+  data?: T;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  status?: number;
+};
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+type ErrorResponse = ActionResponse<undefined> & { success: false };
+
+type ApiErrorResponse = NextResponse<ErrorResponse>;
+type ApiREsponse<T = null> = NextResponse<SuccessResponse<T>>;
